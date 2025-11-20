00f271cb3b489c6771e07be296fd4306bed63378
wo-oaw <wo-oaw@naver.com>
2025-11-20 19:16:27 +0900

    feat: 정기 송금 설정 삭제 API 구현

--- src/main/java/org/creditto/core_banking/domain/regularremittance/entity/RegularRemittance.java
+++ src/main/java/org/creditto/core_banking/domain/regularremittance/entity/RegularRemittance.java
@@ -9,11 +9,8 @@ import lombok.experimental.SuperBuilder;
 import org.creditto.core_banking.domain.account.entity.Account;
 import org.creditto.core_banking.domain.recipient.entity.Recipient;
 import org.creditto.core_banking.global.common.CurrencyCode;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -43,16 +40,6 @@ public abstract class RegularRemittance {
 
     @Enumerated(EnumType.STRING)
     private RegRemStatus regRemStatus;
-
-    // 엔티티 생성 시각 자동 저장
-    @CreatedDate
-    @Column(name = "created_at", nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
-    private LocalDateTime createdAt;
-
-    // 엔티티 수정 시각 자동 저장
-    @LastModifiedDate
-    @Column(name = "updated_at", nullable = false, columnDefinition = "DATETIME(6)")
-    private LocalDateTime updatedAt;
 
     public void updateDetails(
             Account account,
