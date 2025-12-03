986dfd9fa2412cb44f5e63be0b8e85da9256f67c
kswdot <rlatjddms7a@naver.com>
2025-12-03 11:52:03 +0900

    feat: 신용 점수에 따른 환율 우대율 적용

---
+++ src/main/java/org/creditto/core_banking/domain/creditscore/entity/CreditScore.java
@@ -0,0 +1,20 @@
+package org.creditto.core_banking.domain.creditscore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.creditto.core_banking.global.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreditScore extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "score", nullable = false)
+    private Integer score;
+}
